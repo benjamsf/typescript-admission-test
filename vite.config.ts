354ed62c27ac520914af5346b99bc5bd67bedb41
Benjam <53127823+benjamsf@users.noreply.github.com>
2025-06-07 01:41:58 +0300

    fix: gh pages deployment properly

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? "/typescript-admission-test/" : "/",
+  base: "/typescript-admission-test/",
   build: {
     outDir: "docs",
   },
